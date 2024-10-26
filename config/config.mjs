a229edb0d592462b4dee4e1c6a30866e366428c2
abcode <mabsattar@proton.me>
2024-10-26 16:08:45 +0530

    initialized a separate config file, added config class

---
+++ config/config.mjs
@@ -0,0 +1,22 @@
+import yaml from "js-yaml";
+import fs from "node:fs/promises";
+import path from "node:path";
+
+export class Config {
+    constructor() {
+        this.BASE_PATH = path.join(process.cwd(), "config");
+        this.CONFIG_FILE = path.join(this.BASE_PATH, "paths.yaml");
+    }
+
+    async load() {
+        const data = await fs.readFile(this.CONFIG_FILE, "utf8");
+        const config = yaml.load(data);
+        return {
+            sourcifyApi: config.sourcify_repo || "https://repo.sourcify.dev/api",
+            chainId: config.chain_id || "1",
+            ethereumRepo: config.ethereum_repo,
+            batchSize: 10,
+            maxRetries: 3
+        };
+    }
+}
